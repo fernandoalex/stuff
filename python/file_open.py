82adeb07f220531a5a61fc8301af418e8d5ea7df
Fernando <Fernando@Fernandos-MacBook-Pro.local>
2016-03-19 18:44:45 -0300

    Simple sort

--- python/file_open.py
+++ python/file_open.py
@@ -1,9 +1,15 @@
+scores = []
 highest_score = 0
 result_f = open("results.txt")
 for line in result_f:
         (juge, score) = line.split()
+        scores.append(score)
         if score > highest_score:
                 highest_score = score
 result_f.close()
-print("high score:")
-print(highest_score)
+print("high score: %s" % highest_score)
+
+scores.sort(reverse = True)
+
+for i in range(0, 3):
+    print(scores[i])
