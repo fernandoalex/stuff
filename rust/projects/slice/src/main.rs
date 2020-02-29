bd73b7e813f1e9ced3926dd45d8eefc3ed593672
fernandoalex <jfernandoalex@gmail.com>
2020-02-29 16:00:05 +0000

    using slices with the example
    Signed-off-by: fernandoalex <jfernandoalex@gmail.com>

--- rust/projects/slice/src/main.rs
+++ rust/projects/slice/src/main.rs
@@ -2,18 +2,19 @@ fn main() {
     let mut s = String::from("Hello World");
     let word = first_word(&s);
     s.clear();
+    println!("the first word is: {}", word);
 }
 
 // this func is grabbing a reference from a string and return a usize
 // the idea here is to turn the index of the string that contains a white space
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+   &s[..]
 }
