6bd12fee81fdf395653c7da09a46b613703d8814
fernandoalex <jfernandoalex@gmail.com>
2020-03-03 20:22:36 +0000

    more complex match and option
    Signed-off-by: fernandoalex <jfernandoalex@gmail.com>

--- rust/projects/coins/src/main.rs
+++ rust/projects/coins/src/main.rs
@@ -1,16 +1,25 @@
+#[derive(Debug)]
+enum UsState {
+    //Alabama,
+    Alaska,
+}
+
 enum Coin {
-    Penny,
-    Nickel,
+    //Penny,
+    //Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
-        Coin:: Nickel => 5,
+        //Coin::Penny => 1,
+        //Coin:: Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("state quarter from {:?}!", state);
+            25
+        },
     }
 }
 
@@ -18,4 +27,5 @@ fn main() {
     let cent = Coin::Dime;
     
     println!("value is {}", value_in_cents(cent));
+    println!("value is {}", value_in_cents(Coin::Quarter(UsState::Alaska)));
 }
