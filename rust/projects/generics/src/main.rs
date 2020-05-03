a83a06d8271d4d291ae8a5ea173243967efcbf81
fernandoalex <jfernandoalex@gmail.com>
2020-05-03 17:49:44 +0100

    v2
    Signed-off-by: fernandoalex <jfernandoalex@gmail.com>

--- rust/projects/generics/src/main.rs
+++ rust/projects/generics/src/main.rs
@@ -1,5 +1,5 @@
 // Use GIT to see the history of this file
-fn largest_i32(list: &[i32]) -> i32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
@@ -11,25 +11,13 @@ fn largest_i32(list: &[i32]) -> i32 {
     largest
 }
 
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest numern is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("the largest char is {}", result);
 }
