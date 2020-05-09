404c40bc8fe272974b0d905c24f90b13eca2d3e4
fernandoalex <jfernandoalex@gmail.com>
2020-05-09 09:49:30 +0100

    adding test to rectangle
    Signed-off-by: fernandoalex <jfernandoalex@gmail.com>

--- rust/projects/rectangles/src/main.rs
+++ rust/projects/rectangles/src/main.rs
@@ -18,6 +18,25 @@ impl Rectangle {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+           width: 5,
+           height: 1, 
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {  width: 30, height: 50 };
 
