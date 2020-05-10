a434756f499f235825beb5cb49d10ea889d26227
fernandoalex <jfernandoalex@gmail.com>
2020-05-10 10:27:02 +0100

    adding a some examples of tests
    Signed-off-by: fernandoalex <jfernandoalex@gmail.com>

--- rust/projects/guessing_game/src/main.rs
+++ rust/projects/guessing_game/src/main.rs
@@ -2,6 +2,31 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
+
 fn main() {
     println!("Guessing the number!");
 
