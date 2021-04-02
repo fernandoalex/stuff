8a051e4a8d8700659620f30a0372033cbf891681
Fernando Alexandre <jfernandoalex@gmail.com>
2021-04-02 11:02:20 +0100

    example_web_request
    Signed-off-by: Fernando Alexandre <jfernandoalex@gmail.com>

--- rust/projects/example_web_request/src/main.rs
+++ rust/projects/example_web_request/src/main.rs
@@ -1,11 +1,45 @@
-use std::collections::HashMap;
+#![deny(warnings)]
+#![warn(rust_2018_idioms)]
+
+#[macro_use]
+extern crate serde_derive;
+
+use bytes::Buf as _;
+use hyper::Client;
+
+// A simple type alias so as to DRY.
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://ip.jsontest.com/")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
+async fn main() -> Result<()> {
+    let url = "http://jsonplaceholder.typicode.com/users".parse().unwrap();
+    let users = fetch_json(url).await?;
+    // print users
+    println!("users: {:#?}", users);
+
+    // print the sum of ids
+    let sum = users.iter().fold(0, |acc, user| acc + user.id);
+    println!("sum of ids: {}", sum);
     Ok(())
+}
+
+async fn fetch_json(url: hyper::Uri) -> Result<Vec<User>> {
+    let client = Client::new();
+
+    // Fetch the url...
+    let res = client.get(url).await?;
+
+    // asynchronously aggregate the chunks of the body
+    let body = hyper::body::aggregate(res).await?;
+
+    // try to parse as json with serde_json
+    let users = serde_json::from_reader(body.reader())?;
+
+    Ok(users)
+}
+
+#[derive(Deserialize, Debug)]
+struct User {
+    id: i32,
+    name: String,
 }
